fbca96e68ec4ac5d278acf894cd80c9cbf95bd9f
ShintaroKurata <kurata987@gmail.com>
2022-12-31 20:17:41 +0900

    update files

--- wp-content/themes/growweb/entry-post-works.php
+++ wp-content/themes/growweb/entry-post-works.php
@@ -3,6 +3,7 @@
     <?php if ( is_singular() ) { echo '<h1 class="entry-title" itemprop="headline">'; } else { echo '<h2 class="entry-title">'; } ?>
         <?php the_title(); ?>
     <?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
+    <?php if( function_exists( 'aioseo_breadcrumbs' ) ) aioseo_breadcrumbs(); ?>
     <?php if ( !is_search() ) { get_template_part( 'entry', 'post-works-meta' ); } ?>
 </header>
     <?php get_template_part( 'entry', ( is_front_page() || is_home() || is_front_page() && is_home() || is_archive() || is_search() ? 'summary' : 'content-works' ) ); ?>
