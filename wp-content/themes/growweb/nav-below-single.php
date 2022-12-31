64bd4b7bd4571fb72a5109c9fe008929b832d4fc
ShintaroKurata <kurata987@gmail.com>
2022-12-31 20:48:13 +0900

    update files

--- wp-content/themes/growweb/nav-below-single.php
+++ wp-content/themes/growweb/nav-below-single.php
@@ -1,5 +1,5 @@
 <?php $args = array(
-'prev_text' => '<span class="meta-nav">&larr;</span> %title',
-'next_text' => '%title <span class="meta-nav">&rarr;</span>'
+'prev_text' => '<span class="meta-nav"><i class="fa-solid fa-angles-left"></i></span> PREV',
+'next_text' => 'NEXT <span class="meta-nav"><i class="fa-solid fa-angles-right"></i></span>'
 );
 the_post_navigation( $args );
--- wp-content/themes/growweb/nav-below.php
+++ wp-content/themes/growweb/nav-below.php
@@ -1,5 +1,5 @@
 <?php $args = array(
-'prev_text' => sprintf( esc_html__( '%s older', 'growweb' ), '<span class="meta-nav">&larr;</span>' ),
-'next_text' => sprintf( esc_html__( 'newer %s', 'growweb' ), '<span class="meta-nav">&rarr;</span>' )
+'prev_text' => sprintf( esc_html__( '%s older', 'growweb' ), '<span class="meta-nav"><i class="fa-solid fa-angles-left"></i></span>' ),
+'next_text' => sprintf( esc_html__( 'newer %s', 'growweb' ), '<span class="meta-nav"><i class="fa-solid fa-angles-right"></i></span>' )
 );
 the_posts_navigation( $args );
