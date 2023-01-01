eb775be87d914bb635c8f331fc89e44a881d2640
ShintaroKurata <kurata987@gmail.com>
2023-01-02 00:28:59 +0900

    update files

--- wp-content/themes/growweb/archive.php
+++ wp-content/themes/growweb/archive.php
@@ -1,4 +1,4 @@
-<?php get_header("post"); ?>
+<?php get_header("page"); ?>
 <header class="header archive-header">
     <h1 class="archive-works-title" itemprop="name"><?php echo esc_html(get_post_type_object(get_post_type())->label); ?></h1>
     <div class="archive-meta" itemprop="description"><?php if ( '' != get_the_archive_description() ) { echo esc_html( get_the_archive_description() ); } ?></div>
